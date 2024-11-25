4a3f3604ccd3addac9b8a3dd5243549195b590e7
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2024-11-25 15:21:22 -0700

    Trying to ingest data

--- src/test.py
+++ src/test.py
@@ -13,3 +13,16 @@ print("Dependencies installed successfully!")
 
 # we are going to be testing some pyspark using the data we have , cleaning it and transforming it 
 
+# cleaning the data 
+from pyspark.sql import SparkSession
+
+# Initialize Spark Session
+spark = SparkSession.builder.appName("NYC Taxi Trip Prediction").getOrCreate()
+
+# Load datasets
+trip_fare1 = spark.read.csv("./Datasets/trip_fare_1.csv", header=True, inferSchema=True)
+
+
+# Check data : 
+trip_fare1.show(5)
+trip_fare1.printSchema()
