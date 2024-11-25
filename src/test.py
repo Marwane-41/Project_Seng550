987088f399302684bd6cbd60eea6644f01211d05
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2024-11-25 12:16:22 -0700

    create test.py
    test pyspark

---
+++ src/test.py
@@ -0,0 +1,15 @@
+# test pyspark env 
+# Marwane Zaoudi
+
+
+
+
+import pyspark
+import findspark
+print("Dependencies installed successfully!")
+
+
+
+
+# we are going to be testing some pyspark using the data we have , cleaning it and transforming it 
+
