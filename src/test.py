f18f9b7c3d75947df5dc2e349652cf76978d7a82
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2024-12-17 17:38:28 -0700

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,10 +1,9 @@
- 
+
 # Marwane Zaoudi
 import pyspark
 import findspark
 from pyspark.sql.functions import col, when, count
 from pyspark.sql import SparkSession
-
 from pyspark.sql.functions import monotonically_increasing_id
 
 # Initialize Spark Session
@@ -12,12 +11,14 @@ spark = SparkSession.builder \
     .appName("NYC Taxi Trip Processing") \
     .config("spark.driver.memory", "4g") \
     .config("spark.executor.memory", "4g") \
+    .config("spark.sql.files.maxRecordLength", "5368709120") \
     .getOrCreate()
 
-# Load the CSV File
-trip_data = spark.read.csv("/Users/marwanezaoudi/Downloads/trip_data/cleaned_trip_data_3.csv", header=True, inferSchema=True)
 
-# Trim and clean column names to remove leading/trailing whitespaces     , i did this since i figured that the files has a lot of spaces and stuff like that 
+# Load the CSV File
+trip_data = spark.read.csv("/Users/marwanezaoudi/Downloads/trip_data/trip_data_8.csv", header=True, inferSchema=True)
+
+# Trim and clean column names to remove leading/trailing whitespaces     , i did this since i figured that the files has a lot of spaces and couldn't read at first 
 trip_data = trip_data.toDF(*[col_name.strip() for col_name in trip_data.columns])
 
 # Select the columns i want to drop using the drop in pyspark 
@@ -27,24 +28,17 @@ cleaned_data = trip_data.drop(*droppedColumns)
 # creating a new column called id ( increasing numbers )
 increasingId = cleaned_data.withColumn("trip_id", monotonically_increasing_id() + 1 )
 
-
 # Show the first few rows to confirm the changes
-#increasingId.show(20, truncate=False)
-
+increasingId.show(5, truncate=False)
 
 
 # Save the cleaned data
-output_dir = "/Users/marwanezaoudi/Downloads/trip_data/cleaned_trip_data_5.csv"
-increasingId.coalesce(1).write.option("header", "true").csv(output_dir)     # THIS MAKES PYSPARK SAVE ALL PART IN ONE SINGLE FILE .CSV 
+#output_dir = "/Users/marwanezaoudi/Downloads/trip_data/cleaned_trip_data_5.csv"
+#increasingId.coalesce(1).write.option("header", "true").csv(output_dir)     # THIS MAKES PYSPARK SAVE ALL PART IN ONE SINGLE FILE .CSV 
+
+
 
 #print("Data saved successfully!")   # for debugging purposes 
-
 #print(increasingId[increasingId['trip_distance']==0].count())  # for debugging purposes 
 
-
-
-
-
-
-# need to transform and find the individual with customers_i = 0 : 
 
